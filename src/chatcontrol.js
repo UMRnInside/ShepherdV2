d8e85a3bc1060083d0888974ae80a5e39849f732
UMRnInside <30196401+UMRnInside@users.noreply.github.com>
2022-02-23 15:26:11 +0800

    Fix typo

--- src/chatcontrol.js
+++ src/chatcontrol.js
@@ -4,7 +4,7 @@ function addChatPatterns(bot, config) {
     let patterns = config.whisperRegexPatterns;
     for (let i in patterns) {
         console.log(RegExp(patterns[i]));
-        bot.addChatPattern('whisper', RegExp(patterns[i]), {parse: true, deprecate: true} );
+        bot.addChatPattern('whisper', RegExp(patterns[i]), {parse: true, deprecated: true} );
     }
 }
 
@@ -19,7 +19,7 @@ function addChatControl(bot, config) {
     // Wrapper of 'whisper' event
     // See https://github.com/PrismarineJS/mineflayer/issues/2478
     bot.on('whisper', async (username, message) => {
-        console.log(username, message);
+        console.log("(Whisper)", username, message);
         if (username === bot.username) return;
         if (config.allowOwnersOnly && !isOwner(username)) {
             return;
